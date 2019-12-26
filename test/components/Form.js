dcbbc371fe794511d8993079904f4c0fc26c1fdb
dskrenta <dskrenta@gmail.com>
2019-12-26 13:51:26 -0800

    formatting updates

--- test/components/Form.js
+++ test/components/Form.js
@@ -1,40 +1,40 @@
 const Form = {
   render: async () => {
-    return `
+    return /*html*/ `
       <section class="section">
         <div class="field">
-            <p class="control has-icons-left has-icons-right">
-                <input class="input" id="email_input" type="email" placeholder="Enter your Email">
-                <span class="icon is-small is-left">
-                    <i class="fas fa-envelope"></i>
-                </span>
-                <span class="icon is-small is-right">
-                    <i class="fas fa-check"></i>
-                </span>
-            </p>
+          <p class="control has-icons-left has-icons-right">
+            <input class="input" id="email_input" type="email" placeholder="Enter your Email">
+              <span class="icon is-small is-left">
+                <i class="fas fa-envelope"></i>
+              </span>
+              <span class="icon is-small is-right">
+                  <i class="fas fa-check"></i>
+              </span>
+          </p>
         </div>
         <div class="field">
-            <p class="control has-icons-left">
-                <input class="input" id="pass_input" type="password" placeholder="Enter a Password">
-                <span class="icon is-small is-left">
-                    <i class="fas fa-lock"></i>
-                </span>
-            </p>
+          <p class="control has-icons-left">
+            <input class="input" id="pass_input" type="password" placeholder="Enter a Password">
+            <span class="icon is-small is-left">
+              <i class="fas fa-lock"></i>
+            </span>
+          </p>
         </div>
         <div class="field">
-            <p class="control has-icons-left">
-                <input class="input" id="repeat_pass_input" type="password" placeholder="Enter the same Password again">
-                <span class="icon is-small is-left">
-                    <i class="fas fa-lock"></i>
-                </span>
-            </p>
+          <p class="control has-icons-left">
+            <input class="input" id="repeat_pass_input" type="password" placeholder="Enter the same Password again">
+            <span class="icon is-small is-left">
+              <i class="fas fa-lock"></i>
+            </span>
+          </p>
         </div>
         <div class="field">
-            <p class="control">
-                <button class="button is-primary" id="register_submit_btn">
-                Register
-                </button>
-            </p>
+          <p class="control">
+            <button class="button is-primary" id="register_submit_btn">
+              Register
+            </button>
+          </p>
         </div>
       </section>
     `;
@@ -45,13 +45,13 @@ const Form = {
       let pass = document.getElementById('pass_input');
       let repeatPass = document.getElementById('repeat_pass_input');
       if (pass.value != repeatPass.value) {
-          alert (`The passwords dont match`)
+        alert (`The passwords dont match`)
       }
       else if (email.value =='' || pass.value == '' || repeatPass == '') {
-          alert (`The fields cannot be empty`)
+        alert (`The fields cannot be empty`)
       }
       else {
-          alert(`User with email ${email.value} was successfully submitted!`)
+        alert(`User with email ${email.value} was successfully submitted!`)
       }
     });
   }
