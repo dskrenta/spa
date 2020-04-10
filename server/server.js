811a5b4e494c0cfa6770b1a6979def1bc8bcf510
David Skrenta <dskrenta@gmail.com>
2020-04-10 04:31:21 -0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -6,6 +6,4 @@ const port = 3000;
 
 app.use(express.static('src'));
 
-// app.get('/', (req, res) => res.sendFile(path.resolve(`${__dirname}/../test/index.html`)));
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
